7e7fa5e0591a4f7576eef8aeab82b6fe072bebdd
MrBoxs <rizkywanda200155@gmail.com>
2025-10-04 09:18:27 +0700

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,67 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+const eslintConfig = defineConfig(
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		ignores: [
+			"node_modules/**",
+			".next/**",
+			"out/**",
+			"build/**",
+			"next-env.d.ts",
+		],
+	},
+
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		plugins: {
+			"@typescript-eslint": tseslint.plugin,
+		},
+		extends: [
+			...tseslint.configs.recommended,
+			...tseslint.configs.recommendedTypeChecked,
+			...tseslint.configs.stylisticTypeChecked,
+		],
+		rules: {
+			"@typescript-eslint/array-type": "off",
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/consistent-type-imports": [
+				"warn",
+				{ prefer: "type-imports", fixStyle: "inline-type-imports" },
+			],
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/require-await": "off",
+			"@typescript-eslint/no-misused-promises": [
+				"error",
+				{ checksVoidReturn: { attributes: false } },
+			],
+		},
+	},
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: true,
+		},
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	}
+);
 
 export default eslintConfig;
